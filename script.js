3def6b22b30f5910c9f3c6f96d88ff0e5d655bbc
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-05-15 10:49:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // complete the given function
 let isPalindrome = true;
-
-function palindrome(str){
-	let i=0, j=str.length-1;
+let Str = str.toLowerCase();
+function palindrome(Str){
+	let i=0, j=Str.length-1;
 	
 	while(i<=j){
-		if(str[i] !== str[j]){
+		if(Str[i] !== Str[j]){
 			isPalindrome = false
+			return false;
 			break;
 		}
 		i++;
 		j--;
 	}
+	return true;
 }
 
 if(isPalindrome == true){
