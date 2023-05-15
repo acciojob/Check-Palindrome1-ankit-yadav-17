3b7f4739357dea13730a70ba8b0b8f54cc0fa4c8
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-05-15 10:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+	let i=0, j=str.length-1;
+	let isPalindrome = true;
+	while(i<=j){
+		if(str[i] !== str[j]){
+			isPalindrome = false
+			break;
+		}
+		i++;
+		j--;
+	}
 }
 module.exports = palindrome
